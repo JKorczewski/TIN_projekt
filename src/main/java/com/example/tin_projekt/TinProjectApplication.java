dad61a4231f32167f0dfa038f42a63ea87cffb28
Juliusz Korczewski <juliusz.korczewski@gmail.com>
2023-01-10 23:49:35 +0100

    Spring start

---
+++ src/main/java/com/example/tin_projekt/TinProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.tin_projekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tin_projekt/TinProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tin_projekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinProjektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
